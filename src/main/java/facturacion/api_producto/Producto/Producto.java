74edd7d672a68381e4fca9f1bcfbf273e06fcb6f
camilM77 <camilaalison01@gmail.com>
2023-01-14 15:40:56 -0500

    final

---
+++ src/main/java/facturacion/api_producto/Producto/Producto.java
@@ -0,0 +1,16 @@
+package facturacion.api_producto.Producto;import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    private String codigo;
+    private double precio;    
+}
